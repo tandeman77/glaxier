ccf1e2f23407ca918ecd7cb687370fd8f27a8eb8
Tandeman <thawin999@gmail.com>
2020-10-24 17:49:00 +1000

    removed unused testimonials

--- components/TestimonialArea.vue
+++ components/TestimonialArea.vue
@@ -24,68 +24,11 @@
                 congue. Quisque porttitor porttitor tempus.
               </p>
               <div class="author-info">
-                <img src="https://via.placeholder.com/50x50" alt="client" />
-                <h4>Soura</h4>
+                <img src="assets/img/testimonial/f.png" alt="client" />
+                <h4>Somoe Company</h4>
                 <div class="position">
-                  Project Manager
-                  <span>Karmen</span>
-                </div>
-              </div>
-            </div>
-            <!-- end tetimonial item -->
-
-            <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Curabitur leo est, feugiat nec elementum id, suscipit id nulla.
-                Nulla sit amet luctus dolor. Etiam finibus consequat ante ac
-                congue. Quisque porttitor porttitor tempus.
-              </p>
-              <div class="author-info">
-                <img src="https://via.placeholder.com/50x50" alt="client" />
-                <h4>Sara Ezzat</h4>
-                <div class="position">
-                  Project Manager
-                  <span>Ask</span>
-                </div>
-              </div>
-            </div>
-            <!-- end tetimonial item -->
-
-            <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Curabitur leo est, feugiat nec elementum id, suscipit id nulla.
-                Nulla sit amet luctus dolor. Etiam finibus consequat ante ac
-                congue. Quisque porttitor porttitor tempus.
-              </p>
-              <div class="author-info">
-                <img src="https://via.placeholder.com/50x50" alt="client" />
-                <h4>Ahmed Ezzat</h4>
-                <div class="position">
-                  Project Manager
-                  <span>Twitter</span>
-                </div>
-              </div>
-            </div>
-            <!-- end tetimonial item -->
-
-            <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Curabitur leo est, feugiat nec elementum id, suscipit id nulla.
-                Nulla sit amet luctus dolor. Etiam finibus consequat ante ac
-                congue. Quisque porttitor porttitor tempus.
-              </p>
-              <div class="author-info">
-                <img src="https://via.placeholder.com/50x50" alt="client" />
-                <h4>Karim Ezzat</h4>
-                <div class="position">
-                  Project Manager
-                  <span>Facebook</span>
+                  Founder
+                  <span>XXXX</span>
                 </div>
               </div>
             </div>
